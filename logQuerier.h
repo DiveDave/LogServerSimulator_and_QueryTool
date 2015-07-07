e2a4d43f7e7c75d6b683f2d6c17dcc54455e9a0d
DiveDave <yxu60@binghamton.edu>
2015-07-06 21:32:09 -0400

    Create logQuerier.h

---
+++ logQuerier.h
@@ -0,0 +1,19 @@
+/*******************************************************************
+File name		:	logQuerier.h
+Created		:	2015-05-10
+Usage		:	definitions of log file querier
+Owner		:	Yuanda Xu
+*******************************************************************/
+
+#ifndef _LOG_GENERATOR_H
+#define _LOG_GENERATOR_H
+
+
+#include "logFile.h"
+#include "comm.h"
+
+
+
+
+#endif
+
